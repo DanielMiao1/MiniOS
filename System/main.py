dd93e48fe487d6fd235ede3806ab34d7b7cdad11
DanielMiao1 <83146190+DanielMiao1@users.noreply.github.com>
2021-05-02 14:46:56 -0700

    main initializer script for System/main.py

--- System/main.py
+++ System/main.py
@@ -7,9 +7,18 @@ import sys
 
 try:
 	from PyQt5.QtGui import *
+	from PyQt5.QtCore import *
 	from PyQt5.QtWidgets import *
 except ImportError:
 	if input("The PyQt5 Library is not installed. Enter 'install' to install the module, or anything else to stop the process. ").lower() == "install":
 		os.system("pip install PyQt5")
 
 print("Starting the Simplifyc Operating System...") # Print startup message
+
+class Window(QMainWindow):
+	def __init__(self):
+		super(Window, self).__init__()
+
+
+(application, window) = (QApplication(sys.argv), Window())
+application.exec_()
