53ffd806378ca250665cabffb83694cdde938d51
Daniel M <danielmiao2019@icloud.com>
2021-05-02 14:20:27 -0700

    added .gitignore file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# The Simplifyc Operating System
+# Made by Daniel M using PyQt5
 
+try: import os # Import OS module
+except ImportError(name = "os"):
+	print("An error occurred while trying to import the os module.")
+	exit() # If OS module is not found, print error message and exit
+
+try:
+	# Import PyQt5 modules for graphics
+	from PyQt5.QtGui import *
+	from PyQt5.QtWidgets import *
+except ImportError:
+	if input("The PyQt5 Library is not installed. Enter 'install' to install the module, or anything else to stop the process. ").lower() == "install": os.system("pip install PyQt5") # If the PyQt5 main module is not installed using pip, print an error message
