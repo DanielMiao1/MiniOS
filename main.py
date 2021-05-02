5328baf40d89ff21f6901c9385780eb945eb8c7b
DanielMiao1 <83146190+DanielMiao1@users.noreply.github.com>
2021-05-02 14:14:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+# The Simplifyc Operating System
+# Made by Daniel M using PyQt5
+
+try: import os # Import OS module
+except ImportError(name = "os"):
+	print("An error occurred while trying to import the os module.")
+	exit() # If OS module is not found, print error message and exit
+
 try:
-	import os
-	os.system("python System/main.py")
-except Exception as error: print(f"An exception occurred: {error}")
+	# Import PyQt5 modules for graphics
+	from PyQt5.QtGui import *
+	from PyQt5.QtWidgets import *
+except ImportError:
+	if input("The PyQt5 Library is not installed. Enter 'install' to install the module, or anything else to stop the process. ").lower() == "install": os.system("pip install PyQt5") # If the PyQt5 main module is not installed using pip, print an error message
+
