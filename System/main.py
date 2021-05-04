e29320d94fe13ecd7f3f9029388ff95426c010c2
Daniel M <danielmiao2019@icloud.com>
2021-05-04 09:40:19 -0700

    update System/main.py

--- System/main.py
+++ System/main.py
@@ -26,13 +26,15 @@ print("Starting the Simplifyc Operating System...") # Print startup message
 class Window(QMainWindow):
 	def __init__(self):
 		super(Window, self).__init__()
+		self.setMinimumSize(1280, 720)
 		self.dock = QToolBar("Dock") # Create a dock
 		self.dock.setMovable(False) # Make the dock fixed
 		self.dock.setToolButtonStyle(Qt.ToolButtonIconOnly) # Always display icons instead of text in the dock
-		self.dock.setIconSize(QSize(16, 16)) # Configure the dock icon size
+		self.dock.setIconSize(QSize(32, 32)) # Configure the dock icon size
 		self.addToolBar(Qt.BottomToolBarArea, self.dock) # Display the toolbar at the bottom of the screen
-		self.applications = [QAction(QIcon("System/images/browser.png"), "Browser", self)] # Create list of applications
-		self.applications[0].triggered.connect(lambda: os.system("python3 Applications/OSApplications/SimplifycBrowser/main.py")) # Run Applications/OSApplications/SimplifycBrowser/main.py script when the first item in self.applications list is triggered
+		self.applications = [QAction(QIcon("System/images/browser.png"), "Browser", self), QAction(QIcon("System/images/terminal.png"), "Terminal", self)] # Create list of applications
+		self.applications[0].triggered.connect(lambda: os.system("python3 Applications/OSApplications/SimplifycBrowser/main.py")) # Run Applications/OSApplications/SimplifycBrowser/main.py script when the first action in self.applications list is triggered
+		self.applications[1].triggered.connect(lambda: os.system("python3 Applications/OSApplications/SimplifycTerminal/main.py")) # Run Applications/OSApplications/SimplifycTerminal/main.py script when the second action in self.applications list is triggered
 		for i in self.applications: self.dock.addAction(i) # Add each application to the dock
 		self.show() # Show the main window
 
