d74fa68c52287eab5111ddadedb358be1fb99b5e
Daniel M <danielmiao2019@icloud.com>
2021-05-03 21:21:49 -0700

    update System/main.py

--- System/main.py
+++ System/main.py
@@ -5,6 +5,7 @@
 import os
 import sys
 
+# Try to import PyQt5, if PyQt5 is not installed using pip, ask the user to install it
 try: import PyQt5
 except ImportError:
 	if input("The PyQt5 Library is not installed. Enter 'install' to install the module, or anything else to stop the process: ").lower() == "install":
@@ -14,11 +15,11 @@ finally:
 	from PyQt5.QtCore import *
 	from PyQt5.QtWidgets import *
 
+# Check if PyQt5.QtWebEngineWidgets is installed for the browser application, if the module is not installed using pip, ask the user to install it
 try: import PyQt5.QtWebEngineWidgets
 except ImportError:
 	if input("The PyQtWebEngineWidgets Library is not installed. Enter 'install' to install the module, or anything else to stop the process: ").lower() == "install":
 		os.system("pip3 install PyQtWebEngine")
-finally: from PyQt5.QtWebEngineWidgets import *
 
 print("Starting the Simplifyc Operating System...") # Print startup message
 
@@ -32,8 +33,9 @@ class Window(QMainWindow):
 		self.addToolBar(Qt.BottomToolBarArea, self.dock) # Display the toolbar at the bottom of the screen
 		self.applications = [QAction(QIcon("System/images/browser.png"), "Browser", self)] # Create list of applications
 		self.applications[0].triggered.connect(lambda: os.system("python3 Applications/OSApplications/SimplifycBrowser/main.py")) # Run Applications/OSApplications/SimplifycBrowser/main.py script when the first item in self.applications list is triggered
-		for i in self.applications: self.dock.addAction(i)
+		for i in self.applications: self.dock.addAction(i) # Add each application to the dock
 		self.show() # Show the main window
+
 
 (application, window) = (QApplication(sys.argv), Window())
 application.exec_() # Create new Qt application and run Window class
