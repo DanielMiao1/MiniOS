e36a104a948b7341edea746881f195d83175ab51
DanielMiao1 <83146190+DanielMiao1@users.noreply.github.com>
2021-05-11 10:08:46 -0700

    update textedit.py
    update Applications/SimplifycTextEdit/textedit.py

--- Applications/SimplifycTextEdit/textedit.py
+++ Applications/SimplifycTextEdit/textedit.py
@@ -13,12 +13,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWebEngine import *
 
-# application = QApplication(sys.argv) # Create new Qt application
 class TextEdit(QMainWindow):
 	def __init__(self):
 		super(TextEdit, self).__init__()
 		self.web_view = QWebEngineView() # Create new Web Engine View
 		self.web_view.load(QUrl.fromLocalFile(os.path.abspath(os.path.join(os.path.dirname(__file__), "textedit/index.html")))) # Load HTML page at textedit/index.html
-		self.setCentralWidget(self.web_view)
-		self.show() # Show view
-# application.exec_() # Execute Qt application
+		self.setCentralWidget(self.web_view) # Set Central Widget
+		self.show() # Show Window
