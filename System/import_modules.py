b706c407cab12c75c1051a005fca04c0c33dd6dd
Daniel M <danielmiao2019@icloud.com>
2022-08-03 16:43:15 -0700

    fixed python3 executable paths and organized System/widgets subdirectory

--- System/import_modules.py
+++ System/import_modules.py
@@ -6,6 +6,7 @@ Made By Daniel M using Python 3
 """
 
 from os import system
+from sys import executable
 
 from rerun import rerun as rerun_program
 
@@ -17,7 +18,7 @@ def checkModules() -> None:
 	except ImportError:
 		rerun = True
 		if input("The PyQt5 Library is not installed. Enter 'install' to install the module, or anything else to stop the process: ").lower() == "install":
-			system("pip3 install PyQt5")
+			system(f"{executable} -m pip install PyQt5")
 		else:
 			print("You can manually install the PyQt5 Library by running the 'pip3 install PyQt5' command in the terminal")
 			exit()
@@ -28,7 +29,7 @@ def checkModules() -> None:
 	except ImportError:
 		rerun = True
 		if input("The PyQtWebEngineWidgets Library is not installed. Enter 'install' to install the module, or anything else to stop the process: ").lower() == "install":
-			system("pip3 install PyQtWebEngine")
+			system(f"{executable} -m pip install PyQtWebEngine")
 		else:
 			print("You can manually install the PyQtWebEngineWidgets Library by running the 'pip3 install PyQtWebEngine' command in the terminal")
 			exit()
